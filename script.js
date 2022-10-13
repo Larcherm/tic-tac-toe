d2bee638cfbd8d1999a2ecf8d453bb4bb77849ea
miguel <larchermgl@gmail.com>
2022-10-13 14:42:19 +0100

    Refactor modules, add banner logic

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const Player = (name, symbol, index) => {
+const Player = (name = "Player " + index, symbol, index) => {
+    if (!name) {
+        name = "Player " + index;
+    }
     return {
-        name, symbol, index
+        name, symbol, index, rounds:0
     };
 }
 
@@ -8,37 +11,34 @@ const gameBoard = (() => {
     let boardArray = [["", "", ""], ["", "", ""], ["", "", ""]];
     let moves = 0;
     let game_over = false;
-
     const board_cells = document.querySelectorAll(".board-cell");
+    const newRound_button = document.getElementById("newround-button");
+    const restart_button = document.getElementById("restart-button");
+    const result = document.querySelector(".result-message")
+    const banner = document.querySelector(".banner");
+    const board_container = document.getElementById("board-container");
+
     board_cells.forEach(board_cell => board_cell.addEventListener("click", e => {
         let turn = game.getTurn();
         if (addSymbol(board_cell, turn)) {
-            game.alternateTurn();
             if (checkWinner(turn.index)) {
-                game_over = true;
-                const winner = document.createElement("h1");
-                winner.textContent = `${turn.name.toUpperCase()} WON !!!`;
-                document.getElementById("result-announcement").appendChild(winner);
+                postResult(`${turn.name.toUpperCase()} WON !!!`);
+                game.incrementRounds();
+                setBanner();
             }
             else if (moves >= 9) {
-                game_over = true;
-                const tie = document.createElement("h1");
-                tie.textContent = `IT'S A TIE !!!`;
-                document.getElementById("result-announcement").appendChild(tie);
+                postResult("IT'S A TIE !")
             }
-
+            game.alternateTurn();
         };
     })) 
-    const start_button = document.getElementById("start-button");
-    start_button.addEventListener("click", e => {
-        console.log("hey");
-        document.getElementById("board").style.display = "grid";
+    
+    newRound_button.addEventListener("click", e => {
+        resetBoard();
     })
 
-    const restart_button = document.getElementById("restart-button");
-    start_button.addEventListener("click", e => {
-        console.log("hey");
-        resetBoard();
+    restart_button.addEventListener("click", e => {
+        restart();
     })
 
 
@@ -48,7 +48,6 @@ const gameBoard = (() => {
         }
         board_cell.classList.add("used");
         board_cell.textContent = turn.symbol;
-        const classes = board_cell.classList;
         const row = board_cell.dataset.row;
         const column = board_cell.dataset.column;
         boardArray[row][column] = turn.index;
@@ -83,25 +82,55 @@ const gameBoard = (() => {
         }
         return false;
     }
-    return {boardArray};
 
     function resetBoard(){
         let cell_number = 0;
         for (let i = 0, l =  boardArray.length; i < l; i++) {
             for (let j = 0, l2 = boardArray[i].length; j < l2; j++) {
                 boardArray[i][j] = "";
-                document.getElementById("bc" + cell_number).innerHTML = "";
+                let cell = document.getElementById("bc" + cell_number);
+                cell.innerHTML = "";
+                cell.classList.remove("used");
                 cell_number++;
             }
         }
+        game_over = false;
+        moves = 0;
+        result.textContent = "";
     }
+
+    function postResult(message) {
+        game_over = true;
+        result.textContent = message;
+    }
+
+    function setBanner() {
+        banner.style.display = "grid";
+        document.querySelector(".banner-player1").textContent = game.getPlayer1().name;
+        document.querySelector(".banner-player2").textContent = game.getPlayer2().name;
+        document.querySelector(".rounds-player1").textContent = game.getPlayer1().rounds;
+        document.querySelector(".rounds-player2").textContent = game.getPlayer2().rounds;
+    }
+
+    function restart() {
+        banner.style.display = "none";
+        resetBoard();
+        board_container.style.display = "none";
+
+    }
+    return {boardArray, setBanner};
+
 })();
 
 const game = (() => {
-    const player1 = Player("Player 1", "X", 0);
-    const player2 = Player("Player 2", "0", 1);
+    const start_button = document.getElementById("start-button");
+    let turn, player1, player2;
 
-    let turn = player1;
+    start_button.addEventListener("click", e => {
+        document.getElementById("board-container").style.display = "flex";
+        setPlayers();
+        gameBoard.setBanner();
+    })
 
     function alternateTurn() {
         if (turn === player1) {
@@ -118,5 +147,27 @@ const game = (() => {
         return turn;
     }
 
-    return {getTurn, alternateTurn};
+    function getPlayer1() {
+        return player1;
+    }
+
+    function getPlayer2() {
+        return player2;
+    }
+
+    function incrementRounds() {
+        const player_list = [game.getPlayer1(), game.getPlayer2()];
+        for (let player of player_list) {
+            if (player === turn) player.rounds++;
+        }
+        
+    }
+
+    function setPlayers() {
+        player1 = Player(document.getElementById("first-player").value, "X", 1);
+        player2 = Player(document.getElementById("second-player").value, "0", 2);
+        turn = player1;
+    }
+
+    return {getTurn, alternateTurn, getPlayer1, getPlayer2, incrementRounds};
 })();
