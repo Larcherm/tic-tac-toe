8f8f48b2b91a9ce1fdf0c328200f604881d271ea
miguel <larchermgl@gmail.com>
2022-10-17 15:48:14 +0100

    Add first moves response for A.I

--- script.js
+++ script.js
@@ -19,8 +19,13 @@ const gameBoard = (() => {
     const board_container = document.getElementById("board-container");
     const form = document.getElementById("form");
 
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+    })
+
     board_cells.forEach(board_cell => board_cell.addEventListener("click", e => {
         let turn = game.getTurn();
+        console.log("before ai:", moves);       
         if (addSymbol(board_cell, turn)) {
             if (checkWinner(turn.index)) {
                 postResult(`${turn.name.toUpperCase()} WON THE ROUND!!!`);
@@ -31,11 +36,15 @@ const gameBoard = (() => {
                 postResult("IT'S A TIE !")
             }
             game.alternateTurn();
+            incrementMoves();
+            ai.makeMove(game.getTurn(), boardArray, moves, game_over);    
+            console.log("after ai:", moves)        
         };
     })) 
     
     newRound_button.addEventListener("click", e => {
         resetBoard();
+        ai.makeMove(game.getTurn(), boardArray, moves);
     })
 
     restart_button.addEventListener("click", e => {
@@ -52,7 +61,6 @@ const gameBoard = (() => {
         const row = board_cell.dataset.row;
         const column = board_cell.dataset.column;
         boardArray[row][column] = turn.index;
-        moves++;
         return true;
     }
 
@@ -98,6 +106,9 @@ const gameBoard = (() => {
         game_over = false;
         moves = 0;
         result.textContent = "";
+        console.log(game.getTurn().name);
+        ai.makeMove(game.getTurn(), boardArray, moves, game_over);
+
     }
 
     function postResult(message) {
@@ -118,10 +129,24 @@ const gameBoard = (() => {
         banner.style.display = "none";
         resetBoard();
         board_container.style.display = "none";
-
     }
-    return {boardArray, setBanner};
 
+    function updateBoard(newBoard) {
+        let boardIndex = 0;
+        for(let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++){
+                boardArray[i][j] = newBoard[boardIndex];
+                boardIndex++;
+                
+            }
+        }
+    }
+
+    function incrementMoves() {
+        moves++;
+    }
+
+    return {boardArray, setBanner, updateBoard, moves, incrementMoves};
 })();
 
 const game = (() => {
@@ -138,6 +163,7 @@ const game = (() => {
     function alternateTurn() {
         if (turn === player1) {
             turn = player2;
+
             return;
         }
         else if (turn === player2) {
@@ -185,9 +211,49 @@ const game = (() => {
 })();
 
 const ai = (() => { 
-    const board = gameBoard.boardArray;
+    let corners = [0, 2, 6, 8];
+    let middleSides = [1, 3, 5, 7];
 
-    if (!board[1][1]) {
-        return
+
+    function makeMove(turn, board, moves, game_over) {
+        if (turn.ai === false) {
+            return;
+        }
+        console.log("makemove");
+        let newBoard = [];
+        for (let row of board) {
+            for (let cell of row ) {
+                if (!cell) newBoard.push("");
+                else newBoard.push(cell);
+            }
+        }
+        if (moves === 0) {
+            newBoard[4] = 2;
+            addAIsymbol(4, turn.symbol);
+        }
+        else if (moves === 1) {
+            if (!newBoard[4]) {
+                newBoard[4] = 2;
+                addAIsymbol(4, turn.symbol);
+            }
+            else { 
+                let randCorner = Math.floor(Math.random() * (middleSides.length));
+                let corner = corners[randCorner];
+                newBoard[corner] = 2;
+                addAIsymbol(corner, turn.symbol);
+            }
+        }
+        gameBoard.updateBoard(newBoard);
+        gameBoard.incrementMoves();
+        if (!game_over) game.alternateTurn();
+        
     }
+
+    function addAIsymbol(number, symbol) {
+        let cell = document.getElementById("bc" + number);
+        cell.textContent = symbol;
+    }
+
+
+    return {makeMove};
 })()
